52c928855d6f344e17ac250361e3167489ff6120
Saltymeatball <hao100319@gmail.com>
2024-01-22 20:42:02 +0700

    add useRef

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,4 +1,4 @@
-import React, { memo } from 'react'
+import React, { memo, useRef } from 'react'
 import styles from './title.module.scss'
 
 type TitleProps = {
@@ -9,7 +9,18 @@ type TitleProps = {
 }
 
 function Title(address: TitleProps) {
-  return <h1 className={styles.title}>TodoList Typescript</h1>
+  const h1Ref = useRef<HTMLHeadingElement>(null)
+
+  const clickH1 = () => {
+    if (h1Ref.current) {
+      h1Ref.current.style.color = 'red'
+    }
+  }
+  return (
+    <h1 className={styles.title} ref={h1Ref} onClick={clickH1}>
+      TodoList Typescript
+    </h1>
+  )
 }
 
 function equal(prevProps: TitleProps, nextProps: TitleProps) {
